a16c386928bbfcc4ab2770f62e542b9fab03ee02
Francesco Bergonzini <fbergonzinigames@gmail.com>
2020-03-31 12:03:51 +0200

    HELLO WORD BERGO

---
+++ Alloctona_NewBeginning/Assets/TEAM_HELLO_WORD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEAM_HELLO_WORD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("BERGO: Hello Word");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
